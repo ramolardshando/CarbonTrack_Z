ef37624ce83f2c9d44a63d296e7028270ac71310
ramolardshando <ramolardshando@users.noreply.github.com>
2025-11-21 13:32:49 +0000

    Add report-profile.css, static-controller-email.rs, sms-types.java

---
+++ frontend/web/src/router-shared-pages/static-controller-email.rs
@@ -0,0 +1,3 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/carbontrack_z/frontend/web/src/router-shared-pages/static-controller-email.rs
+// Generated at: 2025-11-27T12:25:20.464Z
+
